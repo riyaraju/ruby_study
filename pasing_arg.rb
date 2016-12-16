d4fa860735e576c934cbc00c9e224a81afb60518
riyaraju <riya.raju@mobme.in>
2016-12-16 11:19:05 +0530

    pasing_arg.rb

---
+++ pasing_arg.rb
@@ -0,0 +1,22 @@
+def add(a,b)
+ c=a+b
+ return c
+ 
+end
+def sub(a,b)
+ d=a-b
+ return d
+
+end 
+def mul(num1,num2)
+	num=num1*num2
+	return num
+end
+c=add(20,30)
+puts c
+
+d=sub(30,20)
+ puts d
+
+ num=mul(add(20,30),sub(30,20))
+ puts num
